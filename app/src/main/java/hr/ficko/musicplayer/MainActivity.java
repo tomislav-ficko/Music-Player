f424516812b684138e566511133d22bfb3e09056
tomislav-ficko <ficko.tomislav@gmail.com>
2020-08-24 12:00:24 +0200

    add activity code

---
+++ app/src/main/java/hr/ficko/musicplayer/MainActivity.java
@@ -0,0 +1,31 @@
+package hr.ficko.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playSong1(View view) {
+    }
+    public void playSong2(View view) {
+    }
+    public void playSong3(View view) {
+    }
+    public void playSong4(View view) {
+    }
+    public void playSong5(View view) {
+    }
+    public void playSong6(View view) {
+    }
+    public void playSong7(View view) {
+    }
+    public void playSong8(View view) {
+    }
+}
