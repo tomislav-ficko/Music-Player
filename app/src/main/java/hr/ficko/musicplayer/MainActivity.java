ca4d0d7b6cbc1a1df35aaa62a4a123e272f92c7b
tomislav-ficko <ficko.tomislav@gmail.com>
2020-08-24 12:05:08 +0200

    add constants and variables

--- app/src/main/java/hr/ficko/musicplayer/MainActivity.java
+++ app/src/main/java/hr/ficko/musicplayer/MainActivity.java
@@ -1,10 +1,28 @@
 package hr.ficko.musicplayer;
 
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+
+    private SoundPool mSoundPool;
+    private int mSong1;
+    private int mSong2;
+    private int mSong3;
+    private int mSong4;
+    private int mSong5;
+    private int mSong6;
+    private int mSong7;
+    private int mSong8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
